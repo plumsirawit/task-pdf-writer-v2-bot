6a0ca8da8dd3f8961d47218a383e6a8736ef2c34
Sirawit <plurm2545@hotmail.com>
2023-02-05 13:39:56 +0100

    fix: preserve existing table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS contests;
-
-CREATE TABLE contests (
+CREATE TABLE IF NOT EXISTS contests (
     guild_id VARCHAR(255) NOT NULL,
     git_remote_url TEXT NOT NULL,
   	contest_rel_path TEXT NOT NULL,
